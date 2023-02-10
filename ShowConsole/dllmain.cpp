112af0d3339b89bc0e9f18fb57eef8e0bf6cf260
Hannele Ruiz <justlemoncl@gmail.com>
2023-02-10 05:55:53 -0300

    Added project files

---
+++ ShowConsole/dllmain.cpp
@@ -0,0 +1,12 @@
+BOOL APIENTRY DllMain(HMODULE module, DWORD reason, LPVOID reserved)
+{
+    switch (reason)
+    {
+        case DLL_PROCESS_ATTACH:
+        case DLL_THREAD_ATTACH:
+        case DLL_THREAD_DETACH:
+        case DLL_PROCESS_DETACH:
+            break;
+    }
+    return TRUE;
+}
